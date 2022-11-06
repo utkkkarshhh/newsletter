f03fb029ef1af3daaac5a69d3ef16de22d0e050a
utkarshbhardwaj01 <67866657+utkarshbhardwaj01@users.noreply.github.com>
2022-11-06 13:47:00 +0530

    linked mailchimp and created a new mailing list

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
+const https = require('https');
 const port = 3000;
 
 app.use(express.static("public"));
@@ -11,10 +12,44 @@ app.get('/', (req, res) => {
 })
 
 app.post('/', (req, res) => {
-    var firstName = req.body.fName;
-    var lastName = req.body.lName;
-    var email = req.body.email;
-    console.log(firstName, lastName, email);
+    const firstName = req.body.fName;
+    const lastName = req.body.lName;
+    const email = req.body.email;
+    
+    const data = {
+      members:[{
+      email_address: email,
+        status: "subscribed",
+        merge_fields: {
+          FNAME : firstName,
+          LNAME : lastName  }
+        }]
+      };
+
+    const jsonData = JSON.stringify(data);
+
+    const url = "https://us20.api.mailchimp.com/3.0/lists/2512bc0c5d"
+    const options = {
+      method: 'POST',
+      auth: "utkarsh:bdca8533c4187b75738b5a5987fd3af0-us20"
+    }
+
+    const request = https.request(url, options, function(response){
+
+      if(response.statusCode === 200) {
+        res.sendFile(__dirname + "/success.html");
+      }
+      else
+      res.sendFile(__dirname + "/failure.html");
+
+      response.on("data", function(data){
+        console.log(JSON.parse(data));
+      })
+    })
+
+    request.write(jsonData);
+    request.end();
+
 });
 
 
@@ -25,3 +60,6 @@ app.listen(port, () => {
 
 // API KEY
 // e381dff2829baa134c761423ba487821-us20
+
+// Audience ID
+// 2512bc0c5d
